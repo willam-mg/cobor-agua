eae96310b4f65df72929b28d26b6833df5db25b0
willam-gm <will-gm@hotmail.com>
2023-03-12 09:14:02 -0400

    Crear API para el  Registro y Login de usuario

--- routes/api/v1/api.php
+++ routes/api/v1/api.php
@@ -17,3 +17,10 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::prefix('auth')->group(function() {
+    Route::post('/login', 'AuthController@login');
+    Route::post('/signup', 'AuthController@signup');
+});
+
+
